89f0430df95a0bf6b8f56f87e882368493fe8a67
aatauil <achrafatauil@hotmail.com>
2020-06-19 16:02:08 +0200

    add mobile filter option

--- src/Routes/Products.js
+++ src/Routes/Products.js
@@ -1,16 +1,17 @@
 import React,{useState, useRef}  from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faFilter} from "@fortawesome/free-solid-svg-icons"
+import {faFilter, faTimes} from "@fortawesome/free-solid-svg-icons"
 import FILTERITEMS_QUERY from "../Queries/FILTERITEMS_QUERY"
 import { useQuery } from "@apollo/react-hooks";
 import ProductsList from '../Components/ProductsPage/productsList';
 import { Link } from 'react-router-dom';
 
 function Products(props) {
-    const [showFilter, setShowFilter] = useState(false)
+    const [showFilter, setShowFilter] = useState(true)
     const childRef = useRef();
     const { loading, error, data} = useQuery(FILTERITEMS_QUERY);
 
+    
 
     if (loading) return (
         <section className="my-16 container m-auto">
@@ -52,11 +53,58 @@ function Products(props) {
                 
                 <ProductsList ref={childRef} />
 
+
                 
             </section>
         </section>
     );
     if (error) return `Error! ${error.message}`;
+
+    let filter;
+
+    if(showFilter){
+        filter =
+        <div className="top-0 bg-white w-full h-screen fixed">
+             <button className=" py-4 bg-gray w-full" onClick={() => setShowFilter(false)}> <FontAwesomeIcon icon={faTimes}/> Close Filter</button>
+             <div className="h-auto w-full mr-6 p-6 ">
+                    <div className="">
+                        <label className="block uppercase tracking-wide text-xs font-bold mb-2 mt-6" htmlFor="grid-state">
+                            Model
+                        </label>
+                        <div className="relative">
+                            <select onChange={(e) => childRef.current.refetchModel(e.target.value)}  className="block cursor-pointer appearance-none w-full bg-gray-200 border border-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-dark" id="grid-state">
+                                <option>All</option>
+                                {data.evoheatModels.map(product => (
+                                    <option value={product.id} key={product.id}>{product.Name}</option>
+                                ))}
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                        <label className="block uppercase tracking-wide text-xs font-bold mb-2 mt-6" htmlFor="grid-state">
+                            Use Case
+                        </label>
+                        <div className="relative">
+                            <select onChange={(e) => childRef.current.refetchSuited(e.target.value)} className="block cursor-pointer appearance-none w-full bg-gray-200 border border-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-dark" id="grid-state">
+                                <option>All</option>
+                                {data.bestSuitedFors.map(product => (
+                                    <option value={product.id} key={product.id}>{product.Type}</option>
+                                ))}
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                        <button onClick={() => setShowFilter(false)} className="mt-6 px-6 py-2 bg-black text-white rounded shadow text-sm">Update</button>
+                    </div>
+                    
+                </div>
+        </div>  
+        
+    }
+
+    
 
     return (
         <section className="my-16 container m-auto">
@@ -103,7 +151,7 @@ function Products(props) {
                 </div>
                 
                 <ProductsList ref={childRef} />
-
+                {filter}
             </section>
         </section>
     )
