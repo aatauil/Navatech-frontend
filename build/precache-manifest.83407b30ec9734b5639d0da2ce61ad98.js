89531c69d3c6233ac14e7c32a94de720704ce5c0
aatauil <achrafatauil@hotmail.com>
2020-06-19 13:19:11 +0200

    build project

---
+++ build/precache-manifest.83407b30ec9734b5639d0da2ce61ad98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458f512804f947f891baab118c73a305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccecb751d1ffdc182f8c",
+    "url": "/static/css/main.381c94e6.chunk.css"
+  },
+  {
+    "revision": "401cb50666a4a35586ec",
+    "url": "/static/js/2.c0d0d766.chunk.js"
+  },
+  {
+    "revision": "977f76ca55aa854e22fbdfa4ae2c0753",
+    "url": "/static/js/2.c0d0d766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccecb751d1ffdc182f8c",
+    "url": "/static/js/main.e9aabbd7.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "e79d7fb67fc0252a7db80dd66be7f91b",
+    "url": "/static/media/Aquatic-center.e79d7fb6.png"
+  },
+  {
+    "revision": "7d7770c6cc4e0e93765a24a6243d5878",
+    "url": "/static/media/Force-Features.7d7770c6.png"
+  },
+  {
+    "revision": "b669a0182bcf116ff8744dbd8ecb8f09",
+    "url": "/static/media/Hotel&Resort.b669a018.png"
+  },
+  {
+    "revision": "5b891b892bcc0e6cb3c2bbcb8cf3fc47",
+    "url": "/static/media/Residential.5b891b89.png"
+  },
+  {
+    "revision": "5518d5ceb6231066c6b7f2ac191c0736",
+    "url": "/static/media/Villa.5518d5ce.png"
+  },
+  {
+    "revision": "eefa198244e261d78a6e6296664865b7",
+    "url": "/static/media/gray-circle.eefa1982.svg"
+  }
+]);
