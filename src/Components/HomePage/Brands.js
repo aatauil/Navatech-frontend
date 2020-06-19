6aeeca00e79a972e01abd290f8794fbd3f631ead
aatauil <achrafatauil@hotmail.com>
2020-06-19 13:12:03 +0200

    add privacy consent popup

--- src/Components/HomePage/Brands.js
+++ src/Components/HomePage/Brands.js
@@ -5,7 +5,7 @@ function Brands() {
         <section className="my-20 max-w-screen-xl mx-auto ">
              <div className="flex items-center justify-center">
             <div className="hidden md:inline h-1 bg-gray w-1/3 inline"></div>
-             <h2 className="text-center h2-title w-full md:w-1/4 p-3 whitespace-no-wrap">Brands</h2>
+             <h2 className="text-center h2-title w-full md:w-1/4 whitespace-no-wrap">Brands</h2>
              <div className="hidden md:inline h-1 bg-gray w-1/3 inline"></div>
              </div>
             <div className="w-full flex justify-center mt-6">
