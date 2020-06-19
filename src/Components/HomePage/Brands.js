20697f3f34e2bdfc475bad3cbfd2480a02578a0e
aatauil <achrafatauil@hotmail.com>
2020-06-19 16:29:12 +0200

    fix brand img url

--- src/Components/HomePage/Brands.js
+++ src/Components/HomePage/Brands.js
@@ -9,7 +9,7 @@ function Brands() {
              <div className="hidden md:inline h-1 bg-gray w-1/3 inline"></div>
              </div>
             <div className="w-full flex justify-center mt-6">
-                <img className="w-2/6" src="https://navatech.herokuapp.com/uploads/evo_logo_0fc3fc870f.png" alt="Evoheat logo"></img>
+                <img className="w-2/6" src="https://navatech.herokuapp.com/uploads/Evoheat-logo_7db68520cf.png" alt="Evoheat logo"></img>
             </div>
         </section>
     )
