f77a0aa5593e428eb2561d176cc4ed39d5351ff1
aatauil <achrafatauil@hotmail.com>
2020-06-20 16:40:46 +0200

    contact form fix

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -21,7 +21,45 @@ function Contact() {
                     <h2 className="h2-title text-blue">Availability</h2>
                     <p className="text-center ">Monday - Friday: <br></br> 9 am - 6 pm</p>
                 </div>
-                <input type="hidden" name="contact" value="contact" />
+                <form name="contact-form" action="/" method="POST" data-netlify="true" netlify-honeypot="bot-field" className=" border w-full p-12 border-black row-span-2 col-span-1 md:col-span-2 m-auto mb-16">
+                    <div className="grid  grid-cols-1 md:grid-cols-2 col-gap-20 py-6">
+                        <div className="mt-6">
+                                <label className="font-bold" htmlFor="first-name">First Name:</label>
+                                <div className="my-4">
+                                    <input type="text" name="first-name" id="first-name" className="p-6 mr-2 w-full h-12 bg-gray" ></input>
+                                </div> 
+                        </div>
+                        <div className="mt-6">
+                                <label className="font-bold" htmlFor="electricity">Last Name:</label>
+                                <div className="my-4">
+                                    <input type="text" name="last-name" id="last-name" className="p-6 mr-2 w-full h-12 bg-gray" ></input>
+                                </div> 
+                        </div>
+                        <div className="mt-6">
+                                <label className="font-bold" htmlFor="electricity">Phone Number:</label>
+                                <div className="my-4">
+                                    <input type="tel" name="phone-number:" id="phone-number:" className="p-6 mr-2 w-full h-12 bg-gray" ></input>
+                                </div> 
+                        </div>
+                        <div className="mt-6">
+                                <label className="font-bold" htmlFor="email">E-mail:</label>
+                                <div className="my-4">
+                                    <input type="e-mail" name="email" id="email" className="p-6 mr-2 w-full h-12 bg-gray" ></input>
+                                </div> 
+                        </div>
+                     </div>
+                     <div className="mt-6 flex flex-col">
+                        <label className="font-bold">Message:</label>
+                        <textarea className="p-3 bg-gray  my-4 h-64"></textarea>
+                     </div>
+                     <div className="flex justify-center items-center my-6">
+                         <input type="checkbox" id="privacy" name="privacy" className="mr-4"></input>
+                         <label htmlFor="privacy" className="font-bold">I have read and agreed to the privacy policy of NAVATECH.</label>
+                     </div>
+                     <div className="w-full flex justify-center mt-12">
+                     <input type="submit" className=" px-24 py-4 bg-blue text-white font-bold"></input>
+                     </div>
+                </form>
                 </div>
         </div>
     )
