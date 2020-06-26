e993167c9b037b102c0aebe396a6202d523da6d3
aatauil <achrafatauil@hotmail.com>
2020-06-26 09:22:04 +0200

    different approach on purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,17 @@
-const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-  }
-}
-
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.jsx',
+    './src/**/*.js',
+    './public/index.html'
+  ],
+  css: ['./src/tailwind.css'],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.js"),
-    purgecss({
-      content: ["./src/*.js"],
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-          extensions: ["js"]
-        }
-      ]
-    }),
-    require("autoprefixer")
-  ]
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ],
 };
